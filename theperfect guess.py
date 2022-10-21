9de7e04d7a2b24ac7f005580a32f8bcd4454f6bb
Nikhil Verma  official <78011459+iamnikhilverma@users.noreply.github.com>
2022-10-21 12:22:59 +0530

    Update theperfect guess.py

--- theperfect guess.py
+++ theperfect guess.py
@@ -1,25 +1,31 @@
 import random
 randnumber = random.randint(1,100)
-# print(randnumber)
+print(randnumber)
 userguess = None
 guesses = 0
-
 while (userguess !=randnumber):
-    userguess = int(input("Enter your guess:"))
-    guesses +=1
-    if (userguess==randnumber):
-        print("you guessed it right!")
-    else:
-        if (userguess>randnumber):
-            print("You guessed it wrong! Enter a smaller no")
+    print("To exit enter q")
+    userguess = input("Enter your guess:")
+    try:
+        if userguess=="q":
+            exit()
         else:
-            print("You guessed it wrong! Enter a larger no")
-
-print(f"You guessed the no in {guesses} guesses")
-with open("highscore.txt","r") as f:
-    highscore = int(f.read())
-
-if (guesses<highscore):
-    print("Boom  you broke the highscore")
-    with open("highscore.txt","w") as f:
-        f.write(str(guesses))
+            userguess=int(userguess)
+            guesses +=1
+            if (userguess==randnumber):
+                print("you guessed it right!")
+            else:
+                if (userguess>randnumber):
+                    print("You guessed it wrong! Enter a smaller no")
+                else:
+                    print("You guessed it wrong! Enter a larger no")
+    except Exception as e:
+        print("Invalid value")
+else:
+    print(f"You guessed the no in {guesses} guesses")
+    with open("highscore.txt","r") as f:
+        highscore = int(f.read())
+    if (guesses<highscore):
+        print("Boom  you broke the highscore")
+        with open("highscore.txt","w") as f:
+            f.write(str(guesses))
